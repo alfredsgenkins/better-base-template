ba23ee3e282b273ce2784e5ef97cda86a81e7936
Alfreds Genkins <alfreds@scandiweb.com>
2017-12-10 13:01:36 +0200

    Updated git ignore file. Moved parsing script into separate file.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,19 +1,24 @@
 
-S
-#src/base-template/core/_config.scss,f/5/f537ede459e298d228195e5c5abf1cb07583fda7
-Z
-*src/base-template/core/parts/_columns.scss,5/c/5cc1fda82a0e00dc5e5650c9adb6d93bfccb6585
-Y
-)src/base-template/core/mixins/_color.scss,f/4/f401aab5108567e9c4b751dbc85fa560152b7932
-Y
-)src/base-template/core/mixins/_media.scss,3/9/39679278c7000ccc418d2e5b35edcf64a7e2b849
-W
-'src/base-template/core/extends/_mp.scss,2/e/2e4b4cc4e72a793835fb0a9101fcab9034b25724
-W
-'src/base-template/core/extends/_tf.scss,a/b/abde991c9781c6cfd9560cc421f64798164a8593
 <
 package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
-=
-src/config.js,8/7/87f3e0e4cc62eefaef83559ad73420b22d52e801
-<
-src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
+I
+src/core/extends/_tf.scss,3/6/363731a5093b4484f072145902da65b189e9b85c
+I
+src/core/extends/_mp.scss,c/b/cbf7af7ce15656280fae7b6790b23cfc0b581064
+K
+src/core/mixins/_color.scss,6/5/653dce04a97e1cae69222c04b670dd1382e32aaa
+L
+src/core/parts/_columns.scss,8/a/8a74cf4b007ee92fd911f7da4b6881a54303cf83
+K
+src/core/mixins/_media.scss,0/2/0225c29f8baaa93456146b65ffacea5ed2fce671
+F
+src/base-template.scss,f/5/f5b7358ffcb3551393efd8ceba815be35352fdd6
+9
+	config.js,1/6/1677f29b39bf4fb465d7689c0b6b95fc646fcdbc
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+8
+index.js,a/7/a77b15c0d64cf6111e025645e562faf8ccabb9d7
+:
+
+,gitignore,9/6/96ca8dca8703c809316b08216764c4bcfc0049b6
--- index.js
+++ index.js
@@ -1,85 +1,15 @@
 'use strict';
 
-const config = require('./config.js'),
-    fs = require('fs');
+const config = require('./config'),
+    parser = require('./parser');
 
 let betterBaseTempate = {
-    options: {},
-    scssGenerate: {
-        mixin: (name, params, content) => {
-            return '@mixin'.concat(' ', name, ((params) ? ''.concat(' (', params.join(', '), ')') : ''), ' {', content, '}');
-        },
-        media: (minWidth, maxWidth, content) => {
-            return '@media'.concat(' ', ((minWidth !== 'null') ? ''.concat('(min-width: ', minWidth, ')') : ''), ((minWidth !== 'null' && maxWidth !== 'null') ? 'and' : ''), ((maxWidth !== 'null') ? ''.concat('(max-width: ', maxWidth, ')') : ''), '{', content, '}');
-        },
-        variable: (name, value) => {
-            return '$'.concat(!name ? null : name.replace(/([A-Z])/g, function (g) { return '-' + g[0].toLowerCase() }), ': ', value, ';');
-        }
-    },
-    parseConfig: {
-        variables: () => {
-            let variables = [];
-
-            for (let variable in betterBaseTempate.options) {
-                let value = betterBaseTempate.options[variable];
-
-                if (typeof(value) === 'object') {
-                    let valueParts = [];
-
-                    for (let valuePart in value) {
-                        let valuePartString = '';
-
-                        if (typeof(value[valuePart]) === 'object') {
-                            valuePartString = valuePartString.concat(Object.values(value[valuePart]).join(' '));
-                        } else {
-                            valuePartString = value[valuePart];
-                        }
-
-                        valueParts.push(valuePartString);
-                    }
-
-                    value = valueParts.join(', ');
-                }
-
-                variables.push(betterBaseTempate.scssGenerate.variable(variable, value));
-            }
-
-            fs.writeFile(config.mainPath + '_config.scss', variables.join('\n'), (err) => {
-                if (err) throw err;
-            });
-        },
-        breakpoints: () => {
-            if (betterBaseTempate.options.enableBreakpoints) {
-                let breakpoints = [];
-
-                for (let breakpoint in betterBaseTempate.options.mediaBreakpoints) {
-                    breakpoints.push(
-                        betterBaseTempate.scssGenerate.mixin(
-                            betterBaseTempate.options.mediaBreakpoints[breakpoint].alias,
-                            false,
-                            betterBaseTempate.scssGenerate.media(
-                                betterBaseTempate.options.mediaBreakpoints[breakpoint].minWidth,
-                                betterBaseTempate.options.mediaBreakpoints[breakpoint].maxWidth,
-                                '@content;'
-                            )
-                        )
-                    )
-                }
-
-                fs.writeFile(config.mainPath + 'mixins/_media-generated.scss', breakpoints.join('\n'), (err) => {
-                    if (err) throw err;
-                });
-            }
-        }
-    },
     init: (options) => {
-        betterBaseTempate.options = Object.assign({}, options, config.options);
+        parser.options = Object.assign({}, options, config.options);
 
-        betterBaseTempate.parseConfig.variables();
-        betterBaseTempate.parseConfig.breakpoints();
+        parser.parseConfig.variables();
+        parser.parseConfig.breakpoints();
     }
 };
-
-betterBaseTempate.init();
 
 module.exports = betterBaseTempate;
